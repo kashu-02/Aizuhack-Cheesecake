53f938214000a251d56939db939893f0de33f0ce
kashu <64694079+kashu-02@users.noreply.github.com>
2022-07-18 13:47:04 +0900

    Add definition

---
+++ src/db/definitions/games.js
@@ -0,0 +1,19 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../db";
+
+export default sequelize.define("Users", {
+  gameid: {
+    type: DataTypes.UUID,
+    allowNull: false,
+    primaryKey: true,
+    unique: true,
+  },
+  problemid: {
+    type: DataTypes.UUID,
+    allowNull: false,
+  },
+  starttime: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  }
+});
---
+++ src/db/definitions/problems.js
@@ -0,0 +1,23 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../db";
+
+export default sequelize.define("Users", {
+  problemid: {
+    type: DataTypes.UUID,
+    allowNull: false,
+    primaryKey: true,
+    unique: true,
+  },
+  problem_statement: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  answer_coord: { // 答え(座標)
+    type: DataTypes.GEOMETRY('POINT'),
+    allowNull: false,
+  },
+  answer_name: { // 答え(名前)
+    type: DataTypes.STRING,
+    allowNull: false,
+  }
+});
