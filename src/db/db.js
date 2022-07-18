171b25cbe7b499ea168e53645077c122e1ca25e3
motorailgun <28751910+motorailgun@users.noreply.github.com>
2022-07-18 12:04:31 +0900

    lint

--- src/db/db.js
+++ src/db/db.js
@@ -1,13 +1,10 @@
-import { Sequelize } from 'sequelize';
+import { Sequelize } from "sequelize";
 
-const { DB_NAME: database, 
-        DB_USER: username, 
-        DB_PASS: password } = process.env;
+const { DB_NAME: database, DB_USER: username, DB_PASS: password } = process.env;
 
-export default new Sequelize(database, username, password,
-{
-    host: 'localhost:5432',
-    dialect: 'postgres',
-    logging: false,
-    operatorsAliases: true
-})
+export default new Sequelize(database, username, password, {
+  host: "localhost:5432",
+  dialect: "postgres",
+  logging: false,
+  operatorsAliases: true,
+});
