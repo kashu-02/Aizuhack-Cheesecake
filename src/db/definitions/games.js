5839bba9fe9e9dc47879a1907251657123f52564
motorailgun <28751910+motorailgun@users.noreply.github.com>
2022-07-18 14:45:39 +0900

    テーブル定義を書き直し

--- src/db/definitions/games.js
+++ src/db/definitions/games.js
@@ -1,19 +1,21 @@
 import { DataTypes } from "sequelize";
-import sequelize from "../db";
 
-export default sequelize.define("Users", {
-  gameid: {
-    type: DataTypes.UUID,
-    allowNull: false,
-    primaryKey: true,
-    unique: true,
+export default {
+  name: "Users", 
+  options: {
+    gameid: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+      unique: true,
+    },
+    problemid: {
+      type: DataTypes.UUID,
+      allowNull: false,
+    },
+    starttime: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
   },
-  problemid: {
-    type: DataTypes.UUID,
-    allowNull: false,
-  },
-  starttime: {
-    type: DataTypes.DATE,
-    allowNull: false,
-  },
-});
+};
