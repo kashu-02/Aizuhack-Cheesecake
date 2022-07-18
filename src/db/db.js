a141c855b9b000e4a83c62f8cc2af7b6b9deef5a
motorailgun <28751910+motorailgun@users.noreply.github.com>
2022-07-18 14:44:27 +0900

    DBをwrapするclassを作成

--- src/db/db.js
+++ src/db/db.js
@@ -1,15 +1,27 @@
 import { Sequelize } from "sequelize";
 
-const {
-  DB_NAME: database,
-  DB_USER: username,
-  DB_PASS: password,
-  DB_HOST: host,
-} = process.env;
+// const {
+//   DB_NAME: database,
+//   DB_USER: username,
+//   DB_PASS: password,
+//   DB_HOST: host,
+// } = process.env;
 
-export default new Sequelize(database, username, password, {
-  host: `${host}:5432`,
-  dialect: "postgres",
-  logging: true,
-  operatorsAliases: true,
-});
+// export default new Sequelize(database, username, password, {
+//  host: `${host}:5432`,
+//  dialect: "postgres",
+//  logging: true,
+//  operatorsAliases: true,
+// });
+
+export default class Database {
+  constructor(database, username, password, options){
+    this.sequelize = new Sequelize(database, username, password, options);
+  }
+
+  async createTable(tableName, options){
+    const table = this.sequelize.define(tableName, options);
+    await this.sequelize.sync();
+    return table;
+  }
+}
