ebc7e8c1213bec0c58852c525114146014b77037
kashu <64694079+kashu-02@users.noreply.github.com>
2022-07-18 13:47:04 +0900

    Add definition

--- src/db/definitions/users.js
+++ src/db/definitions/users.js
@@ -1,15 +1,1 @@
-import { DataTypes } from "sequelize";
-import sequelize from "../db";
-
-export default sequelize.define("Users", {
-  userid: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    primaryKey: true,
-    unique: true,
-  },
-  groupid: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-});
+fc95a1a9c6f6fc95a1a9c6f6fc95a1a9c6f6fc95a1a9c6f6fc95a1a9c6f6
