ec8ffaa03d82a01bf3aef1a0dc3388b1990f8767
motorailgun <28751910+motorailgun@users.noreply.github.com>
2022-07-20 13:04:27 +0000

    createGame: DBエラー処理を追加

--- src/group/message/createGame.js
+++ src/group/message/createGame.js
@@ -3,10 +3,18 @@ import crypto from "crypto";
 export default async (event, db) => {
   const groupid = event.source.groupId;
   const gameid = crypto.randomUUID();
+  let reply;
 
-  const exist = await db.groups.findOne({ where: { groupid } });
+  const exist = await db.groups.findOne({ where: { groupid } }).catch((err) => {
+    console.log(err);
+    reply = {
+      type: "text",
+      text: "データベース接続確立エラー",
+    }
+  });
+
   if (exist !== null) {
-    return {
+    reply = {
       type: "text",
       text: "すでに進行中のゲームがあります。終了するには「強制終了」と返信してください",
     };
@@ -15,8 +23,10 @@ export default async (event, db) => {
   await db.groups.create({ groupid, gameid });
 
   console.log(`created game: groupid=${groupid}, gameid=${gameid})}`);
-  return {
+  reply = {
     type: "text",
     text: `ゲームを作成しました: ${gameid}`,
   };
+
+  return reply
 };
