4d6e7d6f5c74312b478657ddcf8abb8cef17d128
kashu <64694079+kashu-02@users.noreply.github.com>
2022-07-20 19:13:02 +0900

    fix server.js

--- src/group/message.js
+++ src/group/message.js
@@ -1,10 +1,22 @@
+import startGame from "./message/startGame.js";
+
 // eslint-disable-next-line no-unused-vars
 export default (event, db) => {
+  let reply;
+  console.log(event.message.text);
   switch (event.message.text) {
-    case "回答":
+    case "ゲーム作成":
       break;
-    case "あああ":
+    case "参加":
+      break;
+    case "ゲーム開始":
+      reply = startGame(event, db);
+      break;
+    case "強制終了":
       break;
     default:
+      break;
   }
+
+  return reply;
 };
