577718e2d13af93c97585a105cfdc977e2641f48
motorailgun <28751910+motorailgun@users.noreply.github.com>
2022-07-20 12:07:50 +0000

    createGame.js: エラー処理等

--- src/group/message/createGame.js
+++ src/group/message/createGame.js
@@ -1,10 +1,22 @@
 import crypto from "crypto";
 
-export default (event, db) => {
-  const { groupid } = event.source;
+export default async (event, db) => {
+  const groupid = event.source.groupId;
   const gameid = crypto.randomUUID();
 
-  db.groups.create({ groupid, gameid });
-  console.log(`created game: ${{ groupid, gameid }}`);
-  return `ゲームを作成しました: ${gameid}`;
+  const exist = await db.groups.findOne({ where: { groupid } });
+  if (exist !== null) {
+    return {
+      type: "text",
+      text: "すでに進行中のゲームがあります。終了するには「強制終了」と返信してください",
+    };
+  }
+
+  await db.groups.create({ groupid, gameid });
+
+  console.log(`created game: groupid=${groupid}, gameid=${gameid})}`);
+  return {
+    type: "text",
+    text: `ゲームを作成しました: ${gameid}`,
+  };
 };
