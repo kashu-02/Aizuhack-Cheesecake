012f2fe05b69ec481919dc20152a77cdc00891f9
motorailgun <28751910+motorailgun@users.noreply.github.com>
2022-07-20 12:07:32 +0000

    message.js: asyncに

--- src/group/message.js
+++ src/group/message.js
@@ -2,17 +2,17 @@ import createGame from "./message/createGame.js";
 import startGame from "./message/startGame.js";
 
 // eslint-disable-next-line no-unused-vars
-export default (event, db) => {
+export default async (event, db) => {
   let reply;
   console.log(event.message.text);
   switch (event.message.text) {
     case "ゲーム作成":
-      reply = createGame(event, db);
+      reply = await createGame(event, db);
       break;
     case "参加":
       break;
     case "ゲーム開始":
-      reply = startGame(event, db);
+      reply = await startGame(event, db);
       break;
     case "強制終了":
       break;
