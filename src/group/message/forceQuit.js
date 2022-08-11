60b69c4b09a5011eb5c5112c85673e6c0ecf7846
motorailgun <28751910+motorailgun@users.noreply.github.com>
2022-08-11 08:22:38 +0000

    forceQuit: each()じゃなくてforEach()ね

--- src/group/message/forceQuit.js
+++ src/group/message/forceQuit.js
@@ -9,7 +9,7 @@ export default async (event, db) => {
   try {
     if (gameid) await db.games.destroy({ where: { gameid } });
     if (users.length > 0) await db.users.destroy({ where: { groupid } });
-    if (users.length > 0) await users.each(async (userid) => { await db.hintquota.destroy({ where: { userid } }) });
+    if (users.length > 0) await users.forEach(async (user) => { await db.hintquota.destroy({ where: { userid: user.dataValues.userid } }) });
     if (groupExist) await db.groups.destroy({ where: { groupid } });
 
     if (!gameid && !users && !groupExist) {
